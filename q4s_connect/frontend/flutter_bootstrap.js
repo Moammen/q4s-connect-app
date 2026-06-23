c727b8532ec1b1320722c00bfa7aeb55d570baae
OsamaR <osama.hon98@gmail.com>
2026-06-24 01:37:14 +0300

    updated frontend
    # Conflicts:
    #	q4s_connect/frontend/flutter_bootstrap.js
    #	q4s_connect/frontend/flutter_service_worker.js
    #	q4s_connect/frontend/main.dart.js
    #	q4s_connect/frontend/main.dart.wasm

--- q4s_connect/frontend/flutter_bootstrap.js
+++ q4s_connect/frontend/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1003375019"
+    serviceWorkerVersion: "1977696539"
   }
 });
